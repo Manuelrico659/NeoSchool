4c73c5063b4208ec3f402bed012572e8804c09c2
manuelrico659 <manuelrico659@gmail.com>
2025-02-26 14:07:48 -0600

    x

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('templates/index.html')
-
+    return render_template("index.html")  # No incluyas "templates/" en la ruta
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
