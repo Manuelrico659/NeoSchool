b72a9f3f768dc70dfd4744de8d0a805ec0b48c28
manuelrico659 <manuelrico659@gmail.com>
2025-02-27 13:22:16 -0600

    read variables.env

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import psycopg2
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 
-load_dotenv("variables.env")
+# Cargar variables de entorno
+load_dotenv(dotenv_path='variables.env')
 
 app = Flask(__name__, template_folder='templates')
 
